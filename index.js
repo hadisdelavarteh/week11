39508305829aa80f3d09899a06d2d9582a0a8de4
hadis <hadis77dlvr@gmail.com>
2026-01-01 01:31:15 -0800

    start

---
+++ index.js
@@ -0,0 +1,6 @@
+const fetchHandler = async () => {
+  const url = "https://dog.ceo/api/breeds/image/random";
+  const res = await fetch(url);
+  const { message } = await res.json();
+  document.querySelector("img").src = message;
+};
